99208cab0d314fef7ab3e047d868059c0fa114a1
jangomax <jangomaxstudio@gmail.com>
2023-08-21 01:21:16 -0700

    Switch to ffmpeg & ffplay

--- api.js
+++ api.js
@@ -5,6 +5,7 @@ const ytdl = require('ytdl-core');
 const { spawn } = require('child_process')
 
 let playing = false;
+let queue = [];
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,7 +16,7 @@ app.get('/stream', async (req, res) => {
         const videoUrl = req.query.url;
         streamYouTubeAudio(videoUrl);
         
-        res.json(playing);
+        res.json({ playing: playing });
     } catch (error) {
         res.status(500).json({ error: 'An error occurred while fetching video info.' });
     }
@@ -29,22 +30,47 @@ app.listen(PORT, () => {
 
 const streamYouTubeAudio = async (url) => {
     if (playing) {
+        queue.push(url);
+        console.log(`Queued: ${url}`);
         return;
     }
     try {
+        console.log(`Now Playing: ${url}`);
         const audioStream = ytdl(url, { filter: 'audioonly' });
 
-        const mpv = spawn('mpv', ['-']);
-        playing = true;
-        audioStream.pipe(mpv.stdin);
+        const ffmpeg = spawn('ffmpeg', [ '-i', 'pipe:0', '-ar', '48000', '-f', 'wav', '-b:a', '256','-ac', '2', 'pipe:1' ]);
+        const ffplay = spawn('ffplay', [ '-i', 'pipe:0', '-nodisp', '-autoexit' ]);
 
-        mpv.on('close', (code) => {
-            console.log(`aplay process exited with code ${code}`);
+        playing = true;
+
+        audioStream.pipe(ffmpeg.stdin);
+        ffmpeg.stdout.pipe(ffplay.stdin);
+
+       ffmpeg.on('error', (err) => {
+            console.log(`ffmpeg error: ${err}`);
+        });
+
+        ffplay.on('error', (err) => {
+            console.log(`ffplay error: ${err}`);
+        });
+
+        ffmpeg.on('close', (code) => {
+            console.log(`ffmpeg process exited with code ${code}`);
             playing = false;
+            if (queue.length > 0) {
+                streamYouTubeAudio(queue.shift());
+            }
+        });
+
+        ffplay.on('close', (code) => {
+            console.log(`ffplay process exited with code ${code}`);
+            playing = false;
+            if (queue.length > 0) {
+                streamYouTubeAudio(queue.shift());
+            }
         });
     } catch (error) {
         console.error('Error streaming audio:', error);
         playing = false;
     }
 };
-

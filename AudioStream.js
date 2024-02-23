d80362384428046ee7b4b16668a26990896c884b
akshtshrma24 <akshitsharmaa65@gmail.com>
2024-02-23 07:08:12 +0000

    No Delays, skip still not working

--- AudioStream.js
+++ AudioStream.js
@@ -1,143 +1,113 @@
-const axios = require('axios');
-const { exec } = require('child_process');
-
 const ytdl = require('ytdl-core');
+const { exec } = require('child_process');
 
 let PAUSE = 'echo \'{ "command": ["set_property", "pause", true] }\' | socat - /tmp/mpvsocket'
 let RESUME = 'echo \'{ "command": ["set_property", "pause", false] }\' | socat - /tmp/mpvsocket'
-let PLAY = 'mpv --no-video --force-window=no --input-ipc-server=/tmp/mpvsocket -'
-let KILL = 'killall mpv && killall ytdl'
-let BASE_URL = "https://www.youtube.com/oembed";
-
 module.exports = class AudioStream {
-  constructor() {
-    this.queue = [];
-    this.playing = false;
-    this.paused = false;
-    this.ytdl = null;
-    this.mpv = null;
-  }
-
-  async updateSign(url) {
-    try {
-        const response = await axios.post(
-            'http://192.168.69.143/api/update-sign',
-            {
-                'text': await this.getTitle(url),
-                'scrollSpeed': '25',
-                'backgroundColor': '#0000ff',
-                'textColor': '#00ff00',
-                'borderColor': '#ff0000',
-                'email': 'sceadmin@sjsu.edu',
-                'firstName': 'SCE'
-            },
-            {
-                headers: {
-                    'Accept-Language': 'en-US,en;q=0.9',
-                    'Connection': 'keep-alive',
-                    'Sec-Fetch-Dest': 'empty',
-                    'Sec-Fetch-Mode': 'cors',
-                    'Sec-Fetch-Site': 'same-origin',
-                    'sec-ch-ua-mobile': '?0',
-                    'sec-ch-ua-platform': '"macOS"'
-                }
-            }
-        );
-    } catch (error) {
-        console.log(error);
+    constructor() {
+        this.mpvs = []
+        this.queue = []
+        this.queuetoshow = []
+        this.total = 0
+        this.isPlaying = false
+        this.current = 0
     }
-  }
-
-  async getTitle(url) {
-      try {
-          const response = await axios.get(`${BASE_URL}?url=${encodeURIComponent(url)}`);
-          return response.data.title;
-      } catch (error) {
-          console.log(error);
-      }
-  }
-
-  async streamYouTubeAudio(url) {
-    if (this.playing || this.paused) {
-      this.queue.push(url);
-      console.log(`Queued: ${url}`);
-      return;
+    
+    newMPV(index) {
+        return exec(`mpv --no-video --force-window=no --input-ipc-server=/tmp/mpvsocket${index} -`)
     }
-    try {
-      console.log(`Now Playing: ${url}`);
-      this.updateSign(url);
-      if(this.ytdl != null){
-        this.ytdl.end();
-        this.ytdl.destroy();
-      }
 
-      this.ytdl = ytdl(url, { filter: 'audioonly' });
-      this.mpv = exec(PLAY)
-      this.playing = true;
-      this.ytdl.pipe(this.mpv.stdin);
-
-      this.mpv.stdout.on('data', (data) => {
-        if(data.toString().includes("End of file")){
-          this.playing  = false
-          this.paused = false
-          this.playNext() 
+    pause(index=-1) {
+        if(index == -1){
+            console.log('pausing', this.current)
+            console.log(`${PAUSE}${this.current}`)
+            return exec(`${PAUSE}${this.current}`)
+        }else{
+            console.log('pausing index', index)
+            return exec(`${PAUSE}${index}`)
         }
-      });
-    } catch (error) {
-      console.error('Error streaming audio:', error);
-      this.playing = false;
     }
-  }
-
-  async pause() {
-    if (!this.paused) {
-      this.ytdl.pause();
-      exec(PAUSE)
-      this.paused = true;
-      console.log('Paused.');
-    }
-  }
-
-  async resume() {
-    if (this.paused) {
-      this.ytdl.resume();
-      exec(RESUME)
-      this.paused = false;
-      console.log('Resumed.');
-    }
-  }
-
-  async skip() {
-    if (this.paused) {
-      this.ytdl.resume();
+    
+    resume(index=-1) {
+        if(index == -1){
+            console.log('resuming', this.current)
+            return exec(`${RESUME}${this.current}`)
+        }else{
+            console.log('resuming index', index)
+            return exec(`${RESUME}${index}`)
+        }
     }
 
-    this.ytdl.end();
-    this.ytdl.destroy();
-    this.mpv.kill();
-    exec(KILL)
-    this.playing = false;
-    // Need to have a sleep statement otherwise it doesnt work 
-    await new Promise(r => setTimeout(r, 100)); 
-    this.playNext();
-  }
-  
-  playNext() {
-    if (this.queue.length > 0)
-      this.streamYouTubeAudio(this.queue.shift());
-  }
+    togglePauseResume() {
+        return exec(`echo '{ "command": ["cycle", "pause"] }' | socat - /tmp/mpvsocket${this.current}`)
+    }
 
-  isPlaying() {
-    return this.playing;
-  }
+    queueUp(url) {
+        if(this.mpvs.length < 2){
+            console.log('first if')
+            this.buffer(url)
+        }
+        else{
+            console.log('second if')
+            this.queue.push(url)
+            console.log(this.queue)
+        }
+        this.queuetoshow.push(url)
+    }
 
-  isPaused() {
-    return this.paused;
-  }
+    play() {
+        let tuple = this.mpvs.shift()
+        if(tuple[1]) this.resume(tuple[1])
+        console.log(this.queue.length)
+        if(this.queue.length > 0) this.buffer(this.queue.shift())
+        this.isPlaying = true
+        this.current = tuple[1]
+    }
 
-  getQueue() {
-    return this.queue;
-  }
+    isPlaying() {
+      return this.isPlaying;
+    }
+
+    getQueue() {
+      return this.queuetoshow;
+    }
+
+    async buffer(url) {
+        this.total += 1
+        let mpv = this.newMPV(this.total)
+        this.mpvs.push([mpv, this.total])
+        
+        let stream = ytdl(url, { filter: 'audioonly' });
+        stream.pipe(mpv.stdin);
+        console.log('here')
+        return new Promise((resolve) => {
+            mpv.stdout.on('data', (data) => {
+                if (data.toString().includes('AO: ')) {
+                    console.log('pausing?')
+                    this.pause(this.total)
+                    if(!this.isPlaying){
+                      this.queuetoshow.shift()
+                      this.play()
+                    }
+                }
+                if (data.toString().includes("Exiting")) {
+                    this.isPlaying = false
+                    this.queuetoshow.shift()
+                    this.play()
+                    resolve()
+                }
+            });
+        })
+    }
+
+    skip() {
+        this.pause(this.current)
+        this.isPlaying = false
+        this.play()
+        return exec(`rm -f /tmp/mpvsocket${this.current} -`)
+    }
+
+    getTotal(){
+        return this.total
+    }
 }
-
-
--- api.js
+++ api.js
@@ -21,39 +21,20 @@ client.collectDefaultMetrics({ register });
 app.post('/stream', bodyParser.json(), async (req, res) => {
   try {
     const videoUrl = req.body.url;
-    audioStream.streamYouTubeAudio(videoUrl);
-    
+    audioStream.queueUp(videoUrl);
     res.json({ playing: audioStream.isPlaying() });
   } catch (error) {
     res.status(500).json({ error: 'An error occurred while fetching video info.' });
   }
 });
 
-app.post('/pause', async (req, res) => {
-  if (!audioStream.isPlaying() || audioStream.isPaused()) {
-    return res.status(400).json({ error: 'Cannot pause. Stream is not playing.' });
-  }
-  audioStream.pause();
-  res.status(200).json('Paused.');
-});
-
-app.post('/resume', async (req, res) => {
-  if (audioStream.isPlaying() && !audioStream.isPaused()) {
-    return res.status(400).json({ error: 'Already playing. '});
-  }
-  if (!audioStream.isPlaying()) {
-    return res.status(400).json({ error: 'Cannot resume. Stream is not playing.' });
-  }
-  audioStream.resume();
-  res.status(200).json('Resumed.');
+app.post('/pauseplay', async (req, res) => {
+  audioStream.togglePauseResume()
+  res.status(200).json('pause/play request sent');
 });
 
 app.post('/skip', async (req, res) => {
-  if (audioStream.getQueue().length == 0 && !audioStream.isPlaying()) {
-    return res.status(400).json({ error: 'Nothing in queue!' });
-  }
-  audioStream.skip();
-  res.status(200).json('Skipped.');
+  res.status(500).json('Not implemented Yet.');
 });
 
 app.get('/queued', async (req, res) => {

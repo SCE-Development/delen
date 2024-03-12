546df86720acb8ea88d03d6384db1a04c98c29e4
akshtshrma24 <akshitsharmaa65@gmail.com>
2024-03-12 17:04:11 +0000

    Removed Delay

--- api.js
+++ api.js
@@ -6,7 +6,7 @@ const client = require('prom-client');
 
 let register = new client.Registry();
 
-const AudioStream = require('./AudioStream');
+const AudioStream = require('./testing');
 let audioStream = new AudioStream();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,62 +15,58 @@ const gauge = new client.Gauge({
   name: 'last_health_check_request',
   help: 'checks the last time the health check was checked',
 });
+
 register.registerMetric(gauge);
 client.collectDefaultMetrics({ register });
 
 app.post('/stream', bodyParser.json(), async (req, res) => {
-  try {
     const videoUrl = req.body.url;
-    audioStream.streamYouTubeAudio(videoUrl);
-    
-    res.json({ playing: audioStream.isPlaying() });
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred while fetching video info.' });
-  }
+    audioStream.queueUp(videoUrl);
+    res.json({ playing: 'queued' });
 });
 
-app.post('/pause', async (req, res) => {
-  if (!audioStream.isPlaying() || audioStream.isPaused()) {
-    return res.status(400).json({ error: 'Cannot pause. Stream is not playing.' });
-  }
-  audioStream.pause();
-  res.status(200).json('Paused.');
-});
-
-app.post('/resume', async (req, res) => {
-  if (audioStream.isPlaying() && !audioStream.isPaused()) {
-    return res.status(400).json({ error: 'Already playing. '});
-  }
-  if (!audioStream.isPlaying()) {
-    return res.status(400).json({ error: 'Cannot resume. Stream is not playing.' });
-  }
-  audioStream.resume();
-  res.status(200).json('Resumed.');
-});
-
-app.post('/skip', async (req, res) => {
-  if (audioStream.getQueue().length == 0 && !audioStream.isPlaying()) {
-    return res.status(400).json({ error: 'Nothing in queue!' });
-  }
-  audioStream.skip();
-  res.status(200).json('Skipped.');
+app.post('/togglePause', async (req, res) => {
+    audioStream.togglePauseResume();
+    res.status(200).json('Paused.');
 });
 
 app.get('/queued', async (req, res) => {
-  return res.status(200).json({ queue: audioStream.getQueue() });
+    return res.status(200).json({ queue: audioStream.getQueue() });
 });
 
+app.post('/skip', async (req, res) => {
+    let resp = audioStream.skip();
+    res.status(200).json({ 'resp ': resp });
+});
+
+app.post('/pause', async (req, res) => {
+    audioStream.pause();
+    res.status(200).json('Paused.');
+});
+
+app.post('/resume', async (req, res) => {
+    audioStream.resume();
+    res.status(200).json('Resumed.');
+});
+
+
+app.get('/total', async (req, res) => {
+    res.json({ total: audioStream.getTotal() })
+})
+
 app.get('/healthCheck', async (req, res) => {
-  gauge.set(Date.now() / 1000);
-  return res.status(200).json({ success: 'True' });
+    gauge.set(Date.now() / 1000);
+    return res.status(200).json({ success: 'True' });
 });
 
 app.get('/metrics', async (request, response) => {
-  response.setHeader('Content-type', register.contentType);
-  response.end(await register.metrics());
+    response.setHeader('Content-type', register.contentType);
+    response.end(await register.metrics());
 });
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
